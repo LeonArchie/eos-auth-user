b62bf347fac4e7cf574a723ca95ab26f5758a115
LeonArchie <levmikhailovish@yandex.ru>
2026-05-13 20:41:49 +0300

    liquibase

---
+++ migrations/scripts/rollbacks/002-triggers.rollback.sql
@@ -0,0 +1,18 @@
+-- ROLLBACK for 002-triggers.sql
+-- Удаляет триггеры и функции
+
+-- Удаляем триггеры
+DROP TRIGGER IF EXISTS trg_cleanup_old_sessions ON sessions;
+DROP TRIGGER IF EXISTS trg_check_session_expiration ON sessions;
+DROP TRIGGER IF EXISTS update_users_timestamp ON users;
+
+-- Удаляем функции
+DROP FUNCTION IF EXISTS cleanup_old_sessions();
+DROP FUNCTION IF EXISTS validate_session_expiration();
+DROP FUNCTION IF EXISTS update_timestamp();
+
+-- Подтверждение
+DO $$
+BEGIN
+    RAISE NOTICE 'Rollback 002 completed: Triggers and functions dropped';
+END $$;
---
+++ migrations/scripts/rollbacks/003-all-privileges.rollback.sql
@@ -0,0 +1,35 @@
+-- ROLLBACK for 003-all-privileges.sql
+-- Откатывает назначение прав (требует осторожности)
+
+-- Удаляем созданные индексы
+DROP INDEX IF EXISTS idx_applied_migrations_name_app;
+DROP INDEX IF EXISTS idx_applied_migrations_applied_at;
+
+-- Удаляем функцию назначения прав
+DROP FUNCTION IF EXISTS grant_applied_migrations_to_user(TEXT);
+
+-- Отзываем права у всех пользователей (кроме администратора)
+DO $$
+DECLARE
+    user_record RECORD;
+BEGIN
+    FOR user_record IN 
+        SELECT rolname 
+        FROM pg_roles 
+        WHERE rolcanlogin = true 
+        AND rolname NOT LIKE 'pg_%'
+        AND rolname != 'postgres'
+        AND rolname != 'db_admin'
+    LOOP
+        BEGIN
+            EXECUTE format('REVOKE ALL PRIVILEGES ON TABLE applied_migrations FROM %I;', user_record.rolname);
+            RAISE NOTICE 'Revoked privileges for: %', user_record.rolname;
+        EXCEPTION
+            WHEN OTHERS THEN
+                RAISE NOTICE 'Could not revoke from %: %', user_record.rolname, SQLERRM;
+        END;
+    END LOOP;
+END $$;
+
+RAISE NOTICE 'Rollback 003: Privileges revoked, but applied_migrations table still exists';
+RAISE NOTICE 'Warning: applied_migrations is managed by Liquibase itself';
